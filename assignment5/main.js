f7ea591ff22dddca2c531064941a025be82fe9f1
khushiverma2310 <khushi.verma1@dcmail.ca>
2026-08-07 20:43:52 -0400

    javascript assignment 5
    editted javascript

--- assignment5/main.js
+++ assignment5/main.js
@@ -4,17 +4,22 @@ const showHideBtn = document.querySelector('.show-hide');
 const commentWrapper = document.querySelector('.comment-wrapper');
 
 commentWrapper.style.display = 'none';
+showHideBtn.setAttribute('aria-expanded', 'false');
 
 showHideBtn.onclick = function() {
   let showHideText = showHideBtn.textContent;
-  if(showHideText === 'Show comments') {
+
+  if (showHideText === 'Show comments') {
     showHideBtn.textContent = 'Hide comments';
     commentWrapper.style.display = 'block';
+    showHideBtn.setAttribute('aria-expanded', 'true');
   } else {
     showHideBtn.textContent = 'Show comments';
     commentWrapper.style.display = 'none';
+    showHideBtn.setAttribute('aria-expanded', 'false');
   }
 };
+
 
 // functionality for adding a new comment via the comments form
 
